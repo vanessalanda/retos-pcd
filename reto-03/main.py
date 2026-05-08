2395d4141774be602e4829c569c2430cf719b0a3
VANESSA FABIOLA LANDA CANALES <vlandac2000@alumno.ipn.mx>
2026-05-07 23:14:02 -0600

    validamos estructuras del programa

--- reto-03/main.py
+++ reto-03/main.py
@@ -1,2 +1,30 @@
 # Reto semana 03
 print ("Reto 3")
+import sys
+def manin ():
+    #diccionario principal 
+    productos = {}
+    
+    primera_linea = True
+    
+    #se lee linea por linea
+    for linea in sys.stdin:
+        linea = linea.strip()
+        if not linea:
+            continue
+        if primera_linea:
+            primera_linea = False
+            continue
+        partes = linea.split(",")
+        if len(partes) !=4:
+            continue
+        
+        producto = partes[1].strip()
+        
+        try:
+            cantidad = int(partes[2])
+            precio_unitario = float(partes[3])
+        except ValueError:
+            continue
+            
+        
